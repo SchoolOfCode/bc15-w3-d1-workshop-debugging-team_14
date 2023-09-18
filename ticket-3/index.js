a752b7eb41d5800a2305307f5bf8f1066b72dcad
chisfy <fyzac96@gmail.com>
2023-09-18 16:35:59 +0100

    ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,21 @@
-function countNumberOfNinesInNumber(number) {
-  let count = 0;
+let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+function countNumberOfNinesInNumber(number) {
+
+const num = String(number)
+.split('')
+.map(Number);
+
+  for (let i = 0; i < num.length; i++) {
+    const digit = num[i];
+    if (digit === 9) {
       count++;
     }
   }
 
   return count;
 }
+
+// let number1 = 23
+console.log(countNumberOfNinesInNumber(111.245));
+// console.log(number1[1]);
