cf656e795d933a8cce3783da33cc8004d806bf7c
Dave-deJong <dndj81@yahoo.co.uk>
2024-03-15 12:22:32 +0000

    Ticket 3 reworked with better variable naming and logging

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,30 +1,9 @@
-// let count = 0;
-
-// function countNumberOfNinesInNumber(number) {
-
-// const num = String(number)
-// .split('')
-// .map(Number);
-
-//   for (let i = 0; i < num.length; i++) {
-//     const digit = num[i];
-//     if (digit === 9) {
-//       count++;
-//     }
-//   }
-
-//   return count;
-// }
-
-// // let number1 = 23
-// console.log(countNumberOfNinesInNumber(111.245));
-// // console.log(number1[1]);
-function countNumberOfNinesInNumber(num) {
+function countNumberOfNinesInNumber(number) {
   let count = 0;
-let number = String (num)
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    console.log(digit)
+  let numberToString = String(number);
+  for (let i = 0; i < numberToString.length; i++) {
+    const digit = numberToString[i];
+    console.log(`Digit to check ${digit}`);
     if ("9" === digit) {
       count++;
     }
@@ -32,4 +11,9 @@ let number = String (num)
 
   return count;
 }
-console.log(countNumberOfNinesInNumber(999.5239))
+let numberToCheck = 999.5239;
+console.log(
+  `Number of 9's in ${numberToCheck} is ${countNumberOfNinesInNumber(
+    numberToCheck
+  )}`
+);
