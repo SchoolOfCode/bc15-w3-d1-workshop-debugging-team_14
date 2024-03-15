2596d0d2f3db4847f23b7fcad7ee8ea897805b1c
Dave-deJong <dndj81@yahoo.co.uk>
2024-03-15 12:41:37 +0000

    refactored the code and made the result logging easier to read.

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,5 +1,7 @@
-function getGradeFromPoints (points) {
-  if ((points) >= 90) {
+// refactored the code, original had syntax errors especially the spelling of points
+
+function getGradeFromPoints(points) {
+  if (points >= 90) {
     return "A";
   } else if (points >= 70) {
     return "B";
@@ -7,10 +9,11 @@ function getGradeFromPoints (points) {
     return "C";
   } else if (points >= 40) {
     return "D";
+  } else {
+    return "E";
   }
-  return "E";
 }
 
-const grade = getGradeFromPoints(40);
-console.log(grade, "should be A");
-
+let pointsAwarded = 39;
+const grade = getGradeFromPoints(pointsAwarded);
+console.log(`${pointsAwarded} points, should be a/an ${grade}`);
