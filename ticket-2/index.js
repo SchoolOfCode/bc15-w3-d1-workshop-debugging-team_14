9427c08ad89a19e85014231b169f9d069171357e
Dave-deJong <dndj81@yahoo.co.uk>
2024-03-15 11:47:46 +0000

    comments on Ticket 2

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,6 +1,8 @@
+// original code had spelling mistakes and wrong syntax
+
 function extractInitialsFromFullName(fullName) {
 /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
-const names = fullName.split(" ");
+const names = fullName.split(" "); // array of names
 let initials = "";
 
 for (let i = 0; i < names.length; i++) {
@@ -10,5 +12,7 @@ initials += currentInitial;
 }
 return initials;
 }
-console.log(extractInitialsFromFullName("Abe Maxwell"))
-console.log(extractInitialsFromFullName("Martin Boxer Harris"))
+console.log(extractInitialsFromFullName("Abe Maxwell"));
+console.log(extractInitialsFromFullName("Martin Boxer Harris"));
+console.log(extractInitialsFromFullName("Felicity Rita Sarah Jones"));
+console.log(extractInitialsFromFullName("Mary Petunia"));
