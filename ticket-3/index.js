ca94816a968162d904bf64b5a17acc31b62e79c6
Dave-deJong <dndj81@yahoo.co.uk>
2024-03-15 11:10:15 +0000

    comments added to ticket 1

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,21 +1,35 @@
-let count = 0;
+// let count = 0;
 
-function countNumberOfNinesInNumber(number) {
+// function countNumberOfNinesInNumber(number) {
 
-const num = String(number)
-.split('')
-.map(Number);
+// const num = String(number)
+// .split('')
+// .map(Number);
 
-  for (let i = 0; i < num.length; i++) {
-    const digit = num[i];
-    if (digit === 9) {
+//   for (let i = 0; i < num.length; i++) {
+//     const digit = num[i];
+//     if (digit === 9) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// // let number1 = 23
+// console.log(countNumberOfNinesInNumber(111.245));
+// // console.log(number1[1]);
+function countNumberOfNinesInNumber(num) {
+  let count = 0;
+let number = String (num)
+  for (let i = 0; i < number.length; i++) {
+    const digit = number[i];
+    console.log(digit)
+    if ("9" === digit) {
       count++;
     }
   }
 
   return count;
 }
-
-// let number1 = 23
-console.log(countNumberOfNinesInNumber(111.245));
-// console.log(number1[1]);
+console.log(countNumberOfNinesInNumber(999.5239))
